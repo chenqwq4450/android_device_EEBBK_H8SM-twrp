5b37a72aec889e5f5a2288bbecd85d61fc7e2ce9
ycly2333 <2780870183@qq.com>
2022-02-06 22:26:55 +0800

    H8SM

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_H8SM.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_H8SM-user \
+    omni_H8SM-userdebug \
+    omni_H8SM-eng
---
+++ omni_H8SM.mk
@@ -0,0 +1,9 @@
+# Inherit from H8SM device
+$(call inherit-product, device/EEBBK/H8SM/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := H8SM
+PRODUCT_NAME := omni_H8SM
+PRODUCT_BRAND := EEBBK
+PRODUCT_MODEL := H8SM
+PRODUCT_MANUFACTURER := EEBBK
